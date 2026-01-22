061b65522c29b471defcce5a5333214704115327
Jason Abbott <jason@toba.app>
2026-01-21 22:50:56 -0700

    update XcodeProj to 9.7.2 for Xcode 26 support (objectVersion 90)

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/modelcontextprotocol/swift-sdk", from: "0.9.0"),
-        .package(url: "https://github.com/tuist/xcodeproj", from: "9.4.2"),
+        .package(url: "https://github.com/tuist/xcodeproj", from: "9.7.2"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.7.0"),
     ],
     targets: [
